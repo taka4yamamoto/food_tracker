901a8243e3171c80a54d5ccd32bc93a27242f1ad
taka4yamamoto <tksymmt.ymtk@gmail.com>
2018-05-11 21:49:20 +0900

    feature: add RaingControll class

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+class RatingControl: UIStackView {
+    //MARK: Initialization
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+}
