bdec64fac95d01045a53e3141b0159447958663a
taka4yamamoto <tksymmt.ymtk@gmail.com>
2018-07-05 19:19:33 +0900

    feature: display meals in table view

--- FoodTracker/MealTableViewCell.swift
+++ FoodTracker/MealTableViewCell.swift
@@ -10,7 +10,7 @@ import UIKit
 class MealTableViewCell: UITableViewCell {
 
     //MARK: Properties
-    @IBOutlet weak var nameLabel: UIView!
+    @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var photoImageView: UIImageView!
     @IBOutlet weak var ratingControl: RatingControl!
     
