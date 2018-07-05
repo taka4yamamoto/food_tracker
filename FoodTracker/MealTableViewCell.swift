b3d1ab743155471085e9b6fbb4a86ed33d436e7a
taka4yamamoto <tksymmt.ymtk@gmail.com>
2018-07-05 17:52:33 +0900

    feature: relate cell in main story board

---
+++ FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by A10 lab Inc. 006 on 2018/07/05.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
